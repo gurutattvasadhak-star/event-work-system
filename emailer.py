5a0dd8f4713bf87ae5dbe98ccfc2bdad999fdbaf
gurutattvasadhak-star <gurutattvasadhak@gmail.com>
2026-03-05 11:27:31 +0530

    Update emailer.py

--- emailer.py
+++ emailer.py
@@ -1,17 +1,21 @@
 import smtplib
 from email.message import EmailMessage
+import os
 
-# CHANGE THESE VALUES
-SMTP_EMAIL = "gurutattvasadhak@gmail.com"
-SMTP_PASSWORD = "yzgq ghnj nhop wrib"
+SMTP_EMAIL = os.getenv("SMTP_EMAIL")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 
 def send_task_email(to_email, subject, body):
+
     msg = EmailMessage()
     msg["From"] = SMTP_EMAIL
     msg["To"] = to_email
     msg["Subject"] = subject
     msg.set_content(body)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(SMTP_EMAIL, SMTP_PASSWORD)
-        server.send_message(msg)
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(SMTP_EMAIL, SMTP_PASSWORD)
+            server.send_message(msg)
+    except Exception as e:
+        print("Email sending failed:", e)
